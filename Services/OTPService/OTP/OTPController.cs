892419dcf3851efb48c407bfa8d2c99d97eb5d82
dhirajpatil <dpdhirajpatil14994@gmail.com>
2025-01-03 18:25:50 +0530

    First commit-basic structure of the application

---
+++ Services/OTPService/OTP/OTPController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OTP
+{
+    [Route("[controller]")]
+    public class OTPController : Controller
+    {
+        private readonly ILogger<OTPController> _logger;
+
+        public OTPController(ILogger<OTPController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
